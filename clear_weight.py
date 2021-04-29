c53f4c6b27839ca5c273a477b4c42a12746e5583
Ssong24 <pub50@naver.com>
2021-04-29 16:52:08 +0900

    write file path rather than just put the path

--- clear_weight.py
+++ clear_weight.py
@@ -8,15 +8,20 @@ except:
     mixed_precision = False  # not installed
 
 device, _ = torch_utils.select_device('0', apex=mixed_precision, batch_size=20)
-ckpt = torch.load('input/pretrained_weights/yolov3-spp-concatv3_nofix.pt', map_location=device)
+file_weight = 'results/kitti/songCylin/kitti_songCylin_concatv1-2_bs24_lr19e-4_e350_img512/weights/best.pt'
+ckpt = torch.load(file_weight, map_location=device)
 
 # Display the ckpt keys (ckpt is dictionary data type)
 # print(ckpt.keys())
 # My pretrained model has data inside ckpt['optimizer'], ckpt['training_results'], ckpt['epoch']
 # For transfer learning, I need to remove those above information in ckpt
-ckpt['optimizer'] = None
-ckpt['training_results'] = None
-ckpt['epoch'] = -1
+print('Epoch info in weight: ', ckpt['epoch'])
+reply = input('Do you want to remove optimizer, training_results, epochs? ')
 
-# Save the checkpoint
-torch.save(ckpt, 'input/pretrained_weights/yolov3-spp-concatv3.pt')
+if reply in ['yes']:
+    ckpt['optimizer'] = None
+    ckpt['training_results'] = None
+    ckpt['epoch'] = -1
+
+    # Save the checkpoint
+    torch.save(ckpt, 'input/pretrained_weights/yolov3-spp-concatv3.pt')
